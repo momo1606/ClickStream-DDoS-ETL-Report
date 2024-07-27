2dc478179536d5d8220c5d0752f4098055497085
Mohammed Noor ul Hasan Kothaliya <mh478572@dal.ca>
2024-07-27 12:44:58 -0300

    hero page changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,12 +11,13 @@ const Hero = (props: Props) => {
         <div className="container px-4 md:px-6 h-full flex flex-col items-center justify-between md:flex-row">
           <div className="space-y-4 text-center md:text-left">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
-              Unlock the Power of Device Data
+               Clickstream Data Brand Report and DDoS detection
             </h1>
             <p className="max-w-[600px] text-lg md:text-xl">
-              Device Stream ETL Analysis helps you effortlessly extract,
-              transform, and analyze data from your connected devices. Unlock
-              valuable insights to drive your business forward.
+              Clickstream ETL Analysis helps you effortlessly extract,
+              transform, and analyze user interaction data from your web
+              applications. Gain valuable insights to enhance user experience
+              and drive business growth.
             </p>
             <div className="flex flex-col gap-2 sm:flex-row justify-center md:justify-start">
               <Button variant={"secondary"} onClick={() => signIn("cognito")}>
